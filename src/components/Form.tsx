f83db0a47e66af34bc5b217a6900660579483b20
Valber Candido <valber_558@hotmail.com>
2023-07-12 18:46:00 -0300

    Requisito-4

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,28 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
-  onCancel: () => void;
+  onCancel: () => void,
+  handleClickPassword: () => void
 }
-function Form({ onCancel }: Props) {
+function Form({ onCancel, handleClickPassword }: Props) {
+  const [nameServ, setNameServ] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [url, setUrl] = useState('');
+  const [btnDisable, SetbtnDisable] = useState(true);
+
+  function resetForm() {
+    setNameServ('');
+    setEmail('');
+    setPassword('');
+    setUrl('');
+  }
+  function handleChangeNameServ(event: React.ChangeEvent<HTMLInputElement>) {
+    setNameServ(event.target.value);
+    handleInfos();
+  }
+
+  function handleChangeLogin(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value);
+    handleInfos();
+  }
+
+  function handleChangePassword(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value);
+    handleInfos();
+  }
+
+  function handleChangeUrl(event: React.ChangeEvent<HTMLInputElement>) {
+    setUrl(event.target.value);
+  }
+
+  function handleInfos() {
+    const regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+    const validPassword = regex.test(password);
+
+    if (nameServ !== '' && email !== '' && password !== '' && validPassword) {
+      SetbtnDisable(false);
+    } else {
+      SetbtnDisable(true);
+    }
+  }
+
   return (
     <form>
       <label>
         Nome do serviço
-        <input type="text" name="Nome do serviço" />
+        <input
+          type="text"
+          name="Serviço"
+          onChange={ handleChangeNameServ }
+          value={ nameServ }
+        />
       </label>
       <label>
         Login
-        <input type="text" name="Login" />
+        <input
+          type="text"
+          name="Login"
+          onChange={ handleChangeLogin }
+          value={ email }
+        />
       </label>
       <label>
         Senha
-        <input type="password" name="Senha" />
+        <input
+          type="password"
+          name="Senha"
+          onChange={ handleChangePassword }
+          value={ password }
+        />
       </label>
       <label>
         URL
-        <input type="text" name="URL" />
+        <input
+          type="text"
+          name="URL"
+          onChange={ handleChangeUrl }
+          value={ url }
+        />
       </label>
-      <button>Cadastrar</button>
+      <button disabled={ btnDisable }>Cadastrar</button>
       <button onClick={ onCancel }>Cancelar</button>
     </form>
   );
