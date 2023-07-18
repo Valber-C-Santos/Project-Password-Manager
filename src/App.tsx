1b19f3e9f8be5dfcafffa0a9ec76d968d1e62ab0
Valber Candido <valber_558@hotmail.com>
2023-07-18 18:08:07 -0300

    Requisito 6 && 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import './App.css';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 
 function App() {
-  const [form, setForm] = useState<boolean>(false);
-  const testeClick = () => {
-    setForm(false);
+  const [btnState, setBtnState] = useState(true);
+  const handleState = () => {
+    setBtnState(!btnState);
   };
-  const testeClick2 = () => {
-    setForm(true);
-  };
-
   return (
     <div>
       <Header />
-      {!form && <button onClick={ testeClick2 }>Cadastrar nova senha</button>}
-      {form && <Form onCancel={ testeClick } />}
+      {!btnState && <Form handleState={ handleState } />}
+      {btnState && <button onClick={ handleState }>Cadastrar nova senha</button>}
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,130 +1,158 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-interface Props {
-  onCancel: () => void,
-}
-function Form({ onCancel }: Props) {
-  const [nameServ, setNameServ] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [url, setUrl] = useState('');
-  const [btnDisable, setbtnDisable] = useState(true);
-  const [hasEight, setHasEight] = useState(false);
-  const [hasSixteen, setHasSixteen] = useState(false);
-  const [hasLetter, setHasLetter] = useState(false);
-  const [hasSpecial, setHasSpecial] = useState(false);
-  const validClass = 'valid-password-check';
-  const invalidClass = 'invalid-password-check';
+type ProphandleState = {
+  handleState: () => void;
+};
 
-  function handleChangeNameServ(event: React.ChangeEvent<HTMLInputElement>) {
-    setNameServ(event.target.value);
-    handleInfos();
-  }
+const inital = {
+  name: '',
+  login: '',
+  password: '',
+  url: '',
+};
 
-  function handleChangeLogin(event: React.ChangeEvent<HTMLInputElement>) {
-    setEmail(event.target.value);
-    handleInfos();
-  }
+type InputProps = {
+  name: string,
+  login: string,
+  password: string,
+  url: string
+};
 
-  function handleChangePassword(event: React.ChangeEvent<HTMLInputElement>) {
-    setPassword(event.target.value);
-    handleInfos();
-  }
+function Form({ handleState }: ProphandleState) {
+  const [cadastro, setCadastro] = useState(false);
+  const [objList, setObjList] = useState<InputProps[]>([]);
+  const [regist, setRegist] = useState(true);
+  const [genericState, setGenericState] = useState<InputProps>(inital);
 
-  function handleChangeUrl(event: React.ChangeEvent<HTMLInputElement>) {
-    setUrl(event.target.value);
-  }
+  const handleInput = ({ target: { name, value } }: any) => {
+    const SaveData = {
+      ...genericState,
+      [name]: value,
+    };
+    setGenericState(SaveData);
+    verifyData(genericState);
+  };
 
-  function handleInfos() {
-    const hasLettersAndNumbers = /[a-zA-Z]/.test(password) && /\d/.test(password);
-    const hasSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-    const hasAtLeastEight = password.length >= 8;
-    const hasLessThanSixteen = hasAtLeastEight && password.length < 16;
-    const validPassword = hasLettersAndNumbers
-      && hasSpecialCharacter
-      && hasAtLeastEight
-      && hasLessThanSixteen;
-    if (hasAtLeastEight) {
-      setHasEight(true);
+  const verifyData = (data: any) => {
+    const { name, login, password } = data;
+    if (name.length && login.length && verifypassword(password)) {
+      setRegist(false);
     } else {
-      setHasEight(false);
+      setRegist(true);
     }
-    if (hasLessThanSixteen) {
-      setHasSixteen(true);
-    } else {
-      setHasSixteen(false);
-    }
-    if (hasLettersAndNumbers) {
-      setHasLetter(true);
-    } else {
-      setHasLetter(false);
-    }
-    if (hasSpecialCharacter) {
-      setHasSpecial(true);
-    } else {
-      setHasSpecial(false);
-    }
+  };
 
-    if (nameServ !== '' && email !== '' && password !== '' && validPassword) {
-      setbtnDisable(false);
-    } else {
-      setbtnDisable(true);
+  const regexLN = /(?=.*[0-9])(?=.*[a-zA-Z])/;
+  const characterSpecial = /(?=.*[!#@$%&])/;
+
+  const verifypassword = (senha: any) => {
+    const regex = /(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-zA-Z])/;
+    const passLength = (senha.length >= 8 && senha.length <= 16);
+    return regex.test(senha) && passLength;
+  };
+
+  const handleApaga = (indice: any) => {
+    const attSenha = [...objList];
+    attSenha.splice(indice, 1);
+    setObjList(attSenha);
+    if (attSenha.length === 0) {
+      setCadastro(false);
     }
-  }
+  };
+
+  const handleCadastro = () => {
+    const addCadastro = { ...genericState };
+    setObjList([...objList, addCadastro]);
+    setCadastro(true);
+    setGenericState(inital);
+  };
 
   return (
-    <form>
-      <label>
+    <>
+      <label htmlFor="name">
         Nome do serviço
         <input
+          id="name"
           type="text"
-          name="Serviço"
-          onChange={ handleChangeNameServ }
-          value={ nameServ }
+          name="name"
+          onChange={ handleInput }
+          value={ genericState.name }
         />
       </label>
-      <label>
+      <label htmlFor="login">
         Login
         <input
+          id="login"
           type="text"
-          name="Login"
-          onChange={ handleChangeLogin }
-          value={ email }
+          name="login"
+          onChange={ handleInput }
+          value={ genericState.login }
         />
       </label>
-      <label>
+      <label htmlFor="senha">
         Senha
         <input
+          id="senha"
           type="password"
-          name="Senha"
-          onChange={ handleChangePassword }
-          value={ password }
+          name="password"
+          onChange={ handleInput }
+          value={ genericState.password }
         />
       </label>
-      <label>
+      <label htmlFor="url">
         URL
         <input
+          id="url"
           type="text"
-          name="URL"
-          onChange={ handleChangeUrl }
-          value={ url }
+          name="url"
+          onChange={ handleInput }
+          value={ genericState.url }
         />
       </label>
-      <button disabled={ btnDisable }>Cadastrar</button>
-      <button onClick={ onCancel }>Cancelar</button>
-      <p className={ hasEight ? validClass : invalidClass }>
-        Possuir 8 ou mais caracteres
-      </p>
-      <p className={ hasSixteen ? validClass : invalidClass }>
-        Possuir até 16 caracteres
-      </p>
-      <p className={ hasLetter ? validClass : invalidClass }>
-        Possuir letras e números
-      </p>
-      <p className={ hasSpecial ? validClass : invalidClass }>
-        Possuir algum caractere especial
-      </p>
-    </form>
+      <button disabled={ regist } onClick={ handleCadastro }>Cadastrar</button>
+      <button onClick={ handleState }>Cancelar</button>
+      <article>
+        {characterSpecial.test(genericState.password)
+          && <h5 className="valid-password-check">Possuir algum caractere especial</h5>}
+        {characterSpecial.test(genericState.password) === false
+          && <h5 className="invalid-password-check">Possuir algum caractere especial</h5>}
+        {regexLN.test(genericState.password)
+          && <h5 className="valid-password-check">Possuir letras e números</h5>}
+        {regexLN.test(genericState.password) === false
+          && <h5 className="invalid-password-check">Possuir letras e números</h5>}
+        {genericState.password.length >= 8
+          && <h5 className="valid-password-check">Possuir 8 ou mais caracteres</h5>}
+        {genericState.password.length < 8
+          && <h5 className="invalid-password-check">Possuir 8 ou mais caracteres</h5>}
+        {genericState.password.length <= 16
+          && <h5 className="valid-password-check">Possuir até 16 caracteres</h5>}
+        {genericState.password.length > 16
+          && <h5 className="invalid-password-check">Possuir até 16 caracteres</h5>}
+      </article>
+      {objList.length === 0
+        && <h5>nenhuma senha cadastrada</h5>}
+      {cadastro === true && (
+        <>
+          <ol>
+            {objList.map((data, indice) => (
+              <li key={ indice }>
+                <a target="_blank" href={ data.url } rel="noreferrer">{ data.name }</a>
+                <h5>{data.password}</h5>
+                <h5>{data.login}</h5>
+                <button
+                  data-testid="remove-btn"
+                  onClick={ () => handleApaga(indice) }
+                >
+                  X
+
+                </button>
+              </li>
+            ))}
+          </ol>
+          <button onClick={ () => setCadastro(false) }>Cadastrar nova senha</button>
+        </>
+      )}
+    </>
   );
 }
 
